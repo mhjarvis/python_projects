2117f13271e65e0e827f7f77f37c6e2c8e972ec8
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-10-29 00:09:07 -0500

    Add 4.1

---
+++ Learning/Python Crash Course/Chapter 4/4.1.py
@@ -0,0 +1,15 @@
+print('')
+pizzas = ['Pepperoni', 'Cheese', 'Sausage']
+
+for pizza in pizzas:
+    print('I like', pizza, 'pizza.')
+
+print('I really love pizza.')
+print()
+
+animals = ['Dog', 'Cat', 'Rat']
+
+for animal in animals:
+    print(f"A {animal} would make a great pet")
+
+print('Any of these animals make great pets.')
