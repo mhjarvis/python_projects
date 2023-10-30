12cdae7e75622e1ec0f1f8259e9752d7581788ff
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-10-30 11:51:31 -0500

    5.2 Added

---
+++ Learning/Python Crash Course/Chapter 5/5.2.py
@@ -0,0 +1,10 @@
+# 5.3 Alien Colors #1
+
+alien_color = 'd'
+
+if alien_color == 'green':
+    print('\nYou earned 5 points')
+elif alien_color != 'green':
+    print('\nYou just earned 10 points.')
+else:
+    print('Oh well')
