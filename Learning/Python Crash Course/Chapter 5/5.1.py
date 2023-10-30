c23d9d1777f076f0a2f5146b828054b6f4182cef
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-10-30 11:39:00 -0500

    Add 5.1.py

---
+++ Learning/Python Crash Course/Chapter 5/5.1.py
@@ -0,0 +1,15 @@
+# 5.1 Conditional Teats
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+# 5.2 More Conditional Tests
+
+print('str' == 'string')
+
+
+print()
