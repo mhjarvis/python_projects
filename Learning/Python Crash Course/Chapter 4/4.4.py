099f504e253f2d68d4d52694b80f1ec1b094aad8
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-10-29 20:46:26 -0500

    Add 4.4

---
+++ Learning/Python Crash Course/Chapter 4/4.4.py
@@ -0,0 +1,23 @@
+# 4.13. Buffeg
+
+# Define tuple
+tup = ('Dog', 'Cat', 'Rat', 'Mice', 'Vermin')
+print()
+for t in tup:
+    print(t)
+
+# Attempt to change tuple
+
+#tup[0] = 'FatDog'
+
+# for t in tup:
+#    print(t)
+
+# Redefine tuple
+
+tup = ('FatCat', 'FatDog', 'Rat', 'Mice', 'Vermin')
+print()
+for t in tup:
+    print(t)
+
+print()
