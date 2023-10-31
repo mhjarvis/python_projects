af7dad3ddab075b343115989b106f1b58766e9dd
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-10-31 01:37:46 -0500

    update 6.1

---
+++ Learning/Python Crash Course/Chapter 6/6.1.py
@@ -0,0 +1,14 @@
+print("\n# 6.1. Person")
+print('---------------------------------------------')
+person = {
+    'first_name': 'Jim',
+    'last_name': 'Smith',
+    'age': 22,
+    'city': 'Sims',
+}
+
+for obj in person:
+    print(person[obj])
+
+print()
+print(person)
